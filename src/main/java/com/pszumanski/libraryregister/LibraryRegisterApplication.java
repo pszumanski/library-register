c92549f7d28a150a398422980a6e4d6522231661
pszumanski <piotr.szumanski5@gmail.com>
2023-12-05 15:10:56 +0100

    Initial commit

---
+++ src/main/java/com/pszumanski/libraryregister/LibraryRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.pszumanski.libraryregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pszumanski/libraryregister/LibraryRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pszumanski.libraryregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
