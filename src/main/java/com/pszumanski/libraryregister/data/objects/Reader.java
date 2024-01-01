dfc0594b4af7e18102a37efd355efcbbf25c23e1
pszumanski <piotr.szumanski5@gmail.com>
2024-01-01 13:59:41 +0100

    Removed unnecessary code

--- src/main/java/com/pszumanski/libraryregister/data/objects/Reader.java
+++ src/main/java/com/pszumanski/libraryregister/data/objects/Reader.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 @ToString
 @RequiredArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Reader implements Comparable {
+public class Reader {
 
     @Id
     @NonNull
@@ -37,11 +37,5 @@ public class Reader implements Comparable {
 
     public String getAddress() {
         return addressFirst + ", " + addressSecond;
-    }
-
-
-    @Override
-    public int compareTo(Object o) {
-        return this.id - ((Reader) o).getId();
     }
 }
